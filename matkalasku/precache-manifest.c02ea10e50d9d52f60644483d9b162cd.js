f18e2a40e0d127bc6dbfa2be1a4a39a23aa7a3af
Jouni K. Seppänen <jks@iki.fi>
2019-05-18 11:06:34 +0300

    Matkalasku

---
+++ matkalasku/precache-manifest.c02ea10e50d9d52f60644483d9b162cd.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e36d6842706f8124fce5be6ce3d736c7",
+    "url": "./index.html"
+  },
+  {
+    "revision": "abca52f3cb8b208e0e45",
+    "url": "./static/css/2.c7bb94bc.chunk.css"
+  },
+  {
+    "revision": "1210a00b1fa813e9a1b7",
+    "url": "./static/css/main.13ae57cf.chunk.css"
+  },
+  {
+    "revision": "abca52f3cb8b208e0e45",
+    "url": "./static/js/2.6fca718b.chunk.js"
+  },
+  {
+    "revision": "1210a00b1fa813e9a1b7",
+    "url": "./static/js/main.e2fdda8f.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  },
+  {
+    "revision": "690eed16f325cbcfebb1932b5cf61c6b",
+    "url": "./static/media/PublicSans-Light.690eed16.woff2"
+  },
+  {
+    "revision": "ef0bab1ac632be42ecd66f27410ff144",
+    "url": "./static/media/PublicSans-Light.ef0bab1a.ttf"
+  },
+  {
+    "revision": "f164ed0024ed068b1a2882c25c8ff33f",
+    "url": "./static/media/PublicSans-Light.f164ed00.woff"
+  },
+  {
+    "revision": "342b80605a0d25e8fe414cfade33cfab",
+    "url": "./static/media/PublicSans-SemiBold.342b8060.woff"
+  },
+  {
+    "revision": "529a386678cb3cda64f032e04cb9da3e",
+    "url": "./static/media/PublicSans-SemiBold.529a3866.woff2"
+  },
+  {
+    "revision": "626e4f2afc0060e0f2f69c6b916cc476",
+    "url": "./static/media/PublicSans-SemiBold.626e4f2a.ttf"
+  },
+  {
+    "revision": "9e421250a44533eaa85b1a392c1f672c",
+    "url": "./static/media/baseline-add-24px.9e421250.svg"
+  },
+  {
+    "revision": "bf808ebb617488b7693e371fe6e2d99e",
+    "url": "./static/media/baseline-delete_forever-24px.bf808ebb.svg"
+  }
+]);
