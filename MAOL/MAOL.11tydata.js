c89b89863102b1393b3f52e66d9ea2f4ca9272ac
Jouni K. Seppänen <jks@iki.fi>
2023-04-30 09:27:29 +0300

    npm dependency update

--- MAOL/MAOL.11tydata.js
+++ MAOL/MAOL.11tydata.js
@@ -1,13 +1,12 @@
 const fs = require("fs"),
-      csv = require("csv-parse/lib/sync"),
-      parse = x => csv(x, {
-          columns: true,
-          skip_empty_lines: true,
-          delimiter: "\t"
-      });
+  { parse } = require("csv-parse/sync"),
+  options = {
+    columns: true,
+    skip_empty_lines: true,
+    delimiter: "\t",
+  };
 
 module.exports = {
-    alkukilpailu: parse(fs.readFileSync("MAOL/alkukilpailu.tsv")),
-    loppukilpailu: parse(fs.readFileSync("MAOL/loppukilpailu.tsv"))
-}
-
+  alkukilpailu: parse(fs.readFileSync("MAOL/alkukilpailu.tsv"), options),
+  loppukilpailu: parse(fs.readFileSync("MAOL/loppukilpailu.tsv"), options),
+};
--- PM/PM.11tydata.js
+++ PM/PM.11tydata.js
@@ -1,12 +1,11 @@
 const fs = require("fs"),
-      csv = require("csv-parse/lib/sync"),
-      parse = x => csv(x, {
-          columns: true,
-          skip_empty_lines: true,
-          delimiter: "\t"
-      });
+  { parse } = require("csv-parse/sync"),
+  options = {
+    columns: true,
+    skip_empty_lines: true,
+    delimiter: "\t",
+  };
 
 module.exports = {
-    kilpailut: parse(fs.readFileSync("PM/kilpailut.tsv"))
-}
-
+  kilpailut: parse(fs.readFileSync("PM/kilpailut.tsv"), options),
+};
