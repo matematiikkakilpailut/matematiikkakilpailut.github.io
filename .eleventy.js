1320916eaa4dc19152c686029b55aeae63936f8e
Jouni K. Seppänen <jks@iki.fi>
2024-02-18 17:30:36 +0200

    Valmennustapahtumien info

--- .eleventy.js
+++ .eleventy.js
@@ -10,16 +10,50 @@ const md = require("markdown-it")({
     level: 2
 }).use(require('markdown-it-attrs'));
 const rss = require("@11ty/eleventy-plugin-rss");
+const cheerio = require('cheerio');
+
 
 module.exports = function (eleventyConfig) {
-  eleventyConfig.addPlugin(eleventyNavigationPlugin);
-  eleventyConfig.addPlugin(rss);
+    eleventyConfig.addPlugin(eleventyNavigationPlugin);
+    eleventyConfig.addPlugin(rss);
 
-  eleventyConfig.addDataExtension("yaml", yaml.load);
-  eleventyConfig.addFilter("markdownify", (x) => {
-    return md.renderInline(x);
-  });
-  eleventyConfig.setLibrary("md", md);
+    eleventyConfig.addDataExtension("yaml", yaml.load);
+    eleventyConfig.addFilter("markdownify", (x) => {
+        return md.renderInline(x);
+    });
+    eleventyConfig.setLibrary("md", md);
+
+    eleventyConfig.addFilter("splitCards", function(content) {
+        const $ = cheerio.load(content, null, false);
+        const sections = [];
+        $('h2').each(function() {
+            const section = $('<div></div>');
+            section.append($(this).clone());
+
+            let sibling = $(this).next();
+            while(sibling.length && !sibling.is('h2')) {
+                section.append(sibling.clone());
+                sibling = sibling.next();
+            }
+            sections.push(section.html());
+        });
+        for (let i = 0; i < sections.length; i++) {
+            const section = sections[i];
+            const header = (
+                i < sections.length - 1 ?
+                    '<div class="col-md-6 col-xl-4 my-3"><div class="card h-100 shadow"><div class="card-body">' :
+                    '<div class="col-xs-12"><div class="card border-info p-2 my-3 shadow"><div class"card-body">'
+            );
+            const trailer = '</div></div></div>';
+            const $ = cheerio.load(header + section + trailer, null, false);
+            $('h2').addClass('card-title');
+            $('p').addClass('card-text');
+            sections[i] = $.html();
+        }
+        const result = sections.join('\n');
+        console.log(result);
+        return result;
+    });
 
   ["default", "page", "seiskat", "valmennus"].map((layout) => {
     eleventyConfig.addLayoutAlias(layout, `${layout}.html`);
